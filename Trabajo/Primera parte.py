54a6d375e6e9955790208254f4192c99372b88d5
Santiago <jerezsanti@gmail.com>
2024-10-05 11:10:23 -0300

    Commit final

--- Trabajo/Primera parte.py
+++ Trabajo/Primera parte.py
@@ -1,16 +1,12 @@
-
-
-n = int(input("Ingrese un numero entero"))
-def num_primo (n):
-    primos = []
-    i = 1 
-    while n< len(primos):
-        for a in range (i):
-            a+=2
-            if i%a==0 and a<i:
-             break 
-            elif a==i:
-                num_primo.append(i)
-            
-    return primos[-1]    
-num_primo(n)
+def num_primo (largo):
+    primos = [2]
+    numero = 1 
+    while largo>len(primos):
+        numero=numero+1
+        for i in primos:
+            if numero%i==0:
+                break
+            elif i>=numero**0.5:
+                primos.append(numero)
+                break
+    return primos[-1]
