4bdd0a2aac26d61a2307a7eef5337ffcc5545955
BiancaDiFranco <biancadifranco373@gmail.com>
2024-10-07 18:43:00 -0300

    detalle

--- Trabajo/Primera parte.py
+++ Trabajo/Primera parte.py
@@ -6,7 +6,9 @@ def num_primo (largo):                  #Definimos la función, llamada "num_pri
         for i in primos:                #Recorremos la lista de primos con el elemento iterable "i", siendo "i" nuestro divisor
             if numero%i==0:             #Si "numero" dividido "i" tiene resto 0 (cero), entonces sabemos que NO es primo y por tanto:
                 break                   #rompemos el bucle y volvemos al incio del loop. Sino pasamos a la siguiente condición:
-            elif i>=numero**0.5:        #si "i" es mayor o igual a la raiz de "numero", entonces encontramos un número primo
+            elif i>=numero**0.5:        #si "i" es mayor o igual a la raiz de "numero", entonces encontramos un número primo. Entendiendo que la raiz cuadrada es el limite de verificación dado que no es necesario chequear todos los divisores acorde a la teoría de los factores en pares.
                 primos.append(numero)   #Añadimos el número primo a la lista
                 break                   #Cortamos el bucle y proseguimos el loop con el siguiente número
     return primos[-1]                   #La función nos devuelve el último número primo de la lista "primos" (aquel que fue solicitado)
+
+
