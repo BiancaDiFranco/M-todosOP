6a1429bdb3f5839aac3d3633e759a553626e406f
BiancaDiFranco <biancadifranco373@gmail.com>
2024-11-23 21:29:04 -0300

    Gráficos

--- Trabajo final/trabajo.py
+++ Trabajo final/trabajo.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 df=pd.read_excel('https://raw.githubusercontent.com/BiancaDiFranco/M-todosOP/main/Trabajo%20final/Rosario%20F.xlsx', engine='openpyxl')
 
@@ -287,3 +289,82 @@ Ninguno_NoSabe_otros = df[filtroO_Ninguno_NoSabe].shape[0]
 print(f"{Ninguno_NoSabe_varones} hombres piensan no votar a ninguno de ellos /n"
        f"{Ninguno_NoSabe_mujeres} mujeres piensan no votar a ninguno de ellos /n"
        f"{Ninguno_NoSabe_otros} otros piensan no votar a ninguno de ellos /n")
+
+
+data = pd.DataFrame({'España' : [826, 943, 942, 901],
+                     'Colombia': [668, 781, 791, 813],
+                     'México': [488, 553, 563, 537]},
+                    index=('Lunes', 'Martes', 'Miercoles', 'Jueves'))
+n = len(data.index)
+x = np.arange(n)
+width = 0.25
+
+
+#Gráfico de barras
+data= pd.DataFrame({"Masculino": [LLA_varones, FdT_varones,JxC_varones,FIT_varones,PNK_varones,Otros_varones, Ninguno_NoSabe_varones],
+                     "Femenino" : [LLA_mujeres,FdT_mujeres,JxC_mujeres,FIT_mujeres,PNK_mujeres,Otros_mujeres, Ninguno_NoSabe_mujeres],
+                     "Otros": [LLA_otros,FdT_otros,JxC_otros,FIT_otros, PNK_otros,Otros_otros, Ninguno_NoSabe_otros]},
+                     index= ("LLA","FdT","JxC","FIT","PNK", "Otros","Ninguno/NS"))
+total = data.sum(axis=1)
+plt.bar(data.index, data.Masculino + data.Femenino + data.Otros, label= "Masculino")
+plt.bar(data.index, data.Femenino + data.Otros, label= "Femenino")
+plt.bar(data.index, data.Otros, label= "Otros")
+plt.legend(loc='best')
+plt.title("Intención de voto por partido político y género")
+plt.show()
+
+#Gráfico de torta, rango etario votantes de LLA
+votantesLLA = [LLA_r1,LLA_r2,LLA_r3,LLA_r4,LLA_r5]
+categorias = ["Entre 16 y 25", "Entre 26 a 35 años", "Entre 36 a 45 años", "Entre 46 a 55 años", "Entre 56 y más años" ]
+plt.pie (votantesLLA, labels=categorias,autopct="%0.1f %%") # Este argumento se usa para mostrar el porcentaje de cada sección. El formato "%0.1f %%" indica que se debe mostrar el porcentaje con un solo decimal
+plt.axis("equal")
+plt.title("Composición etaria de los votantes de La Libertad Avanza")
+plt.show()
+
+#Gráfico de torta, rango etario votantes del Fdt
+votantesFdT = [FdT_r1,FdT_r2,FdT_r3,FdT_r4,FdT_r5]
+categorias = ["Entre 16 y 25", "Entre 26 a 35 años", "Entre 36 a 45 años", "Entre 46 a 55 años", "Entre 56 y más años" ]
+plt.pie (votantesFdT, labels=categorias,autopct="%0.1f %%")
+plt.axis("equal")
+plt.title("Composición etaria de los votantes del Frente de Todos")
+plt.show()
+
+#Gráfico de torta, rango etario votantes de JxC
+votantesJxC = [JxC_r1,JxC_r2,JxC_r3,JxC_r4,JxC_r5]
+categorias = ["Entre 16 y 25", "Entre 26 a 35 años", "Entre 36 a 45 años", "Entre 46 a 55 años", "Entre 56 y más años" ]
+plt.pie (votantesJxC, labels=categorias,autopct="%0.1f %%")
+plt.axis("equal")
+plt.title("Composición etaria de los votantes de Juntos por el Cambio")
+plt.show()
+
+#Gráfico de torta, rango etario votantes del FIT
+votantesFIT = [FIT_r1,FIT_r2,FIT_r3,FIT_r4,FIT_r5]
+categorias = ["Entre 16 y 25", "Entre 26 a 35 años", "Entre 36 a 45 años", "Entre 46 a 55 años", "Entre 56 y más años" ]
+plt.pie (votantesFIT, labels=categorias,autopct="%0.1f %%")
+plt.axis("equal")
+plt.title("Composición etaria de los votantes del FIT")
+plt.show()
+
+#Gráfico de torta, rango etario votantes del PNK
+votantesPNK = [PNK_r1,PNK_r2,PNK_r3,PNK_r4,PNK_r5]
+categorias = ["Entre 16 y 25", "Entre 26 a 35 años", "Entre 36 a 45 años", "Entre 46 a 55 años", "Entre 56 y más años" ]
+plt.pie (votantesPNK, labels=categorias,autopct="%0.1f %%")
+plt.axis("equal")
+plt.title("Composición etaria de los votantes del PNK")
+plt.show()
+
+#Gráfico de torta, rango etario votantes de Otros
+votantesOtros = [Otros_r1,Otros_r2,Otros_r3,Otros_r4,Otros_r5]
+categorias = ["Entre 16 y 25", "Entre 26 a 35 años", "Entre 36 a 45 años", "Entre 46 a 55 años", "Entre 56 y más años" ]
+plt.pie (votantesOtros, labels=categorias,autopct="%0.1f %%")
+plt.axis("equal")
+plt.title("Composición etaria de los votantes de Otros")
+plt.show()
+
+#Gráfico de torta, rango etario de quienes no votan a ninguno o aún no se han decidido
+votantesNinguno_NoSabe = [Ninguno_NoSabe_r1,Ninguno_NoSabe_r2,Ninguno_NoSabe_r3,Ninguno_NoSabe_r4,Ninguno_NoSabe_r5]
+categorias = ["Entre 16 y 25", "Entre 26 a 35 años", "Entre 36 a 45 años", "Entre 46 a 55 años", "Entre 56 y más años" ]
+plt.pie (votantesNinguno_NoSabe, labels=categorias,autopct="%0.1f %%")
+plt.axis("equal")
+plt.title("Composición etaria de quienes no votan a ninguno o aún no se han decidido")
+plt.show()
